04c7cdcc74599966e033b5950d2590e4e4add02d
Brad Cassity <bradley.cassity@gmail.com>
2020-10-17 13:22:34 -0600

    Updated get routes to include data from parties

--- server.js
+++ server.js
@@ -55,7 +55,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // return all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = 'SELECT * FROM candidates';
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   const params = [];
 
   db.all(sql, params, (err, rows) => {
@@ -73,7 +77,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.get(sql, params, (err, row) => {
